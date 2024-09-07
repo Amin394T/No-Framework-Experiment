9b790098a03989c9ff7ab491b5d0361c3a89524d
Amin394 <amin394@outlook.com>
2024-09-07 11:56:38 +0100

    experimented with reactive search inputs

--- components/main.js
+++ components/main.js
@@ -1,106 +1,96 @@
-let counter = {
-  value: 0,
-  updater: function (count) {
-    this.value = count;
-    render();
-  },
-};
+import "../styles/main.css";
+// import Navigation from "./navigation";
+// import Content from "./content";
+// import Feed from "./feed";
 
-function render() {
-  const node = document.querySelector("#root");
-  node.innerHTML = "";
-  node.innerHTML = `
-    <div>
-      <h1>Hello, World!</h1>
-      <button class="counter" type="button">${counter.value}</button>
-      <button class="counter" type="button">${counter.value * 2}</button>
-    </div>
-  `;
-
-  document.querySelectorAll(".counter").forEach((button) => {
-    button.onclick = () => counter.updater(counter.value + 1);
-  });
-}
-render();
-
-/*
-import "../styles/App.css";
-import Navigation from "./Navigation";
-import Content from "./Content";
-import Feed from "./Feed";
-
-let currentBlog = {
-  value: new URLSearchParams(window.location.search).get("blog"),
-  updater: function (blogID) {
-    this.value = blogID;
-    document.querySelectorAll(".blogObserver").forEach((element) => {
-      element.innerHTML = this.value;
-    });
-  },
-};
+// let currentBlog = {
+//   value: new URLSearchParams(window.location.search).get("blog"),
+//   updater: function (blogID) {
+//     this.value = blogID;
+//     render();
+//   },
+// };
 
 let searchWord = {
   value: "",
-  updater: function (query) {
-    this.value = query;
-    document.querySelectorAll(".blogObserver").forEach((element) => {
-      element.innerHTML = this.value;
+  update: (query) => {
+    searchWord.value = query;
+    document.querySelectorAll(".searchConsumer").forEach(element => {
+      element.innerHTML = element.tagName == "DIV" ? searchWord.value : null;
+      element.value = searchWord.value;
     });
   },
 };
 
-function App() {
-  const [currentBlog, setCurrentBlog] = useState(new URLSearchParams(window.location.search).get("blog"));
-  const [searchWord, setSearchWord] = useState("");
+document.querySelector("#root").innerHTML = `
+  <div>
+    <input class="searchObserver searchConsumer" placeholder="🔍  Search ..." value="${searchWord.value}" />
+    <input class="searchObserver searchConsumer" placeholder="🔍  Search ..." value="${searchWord.value}" />
+    <div class="searchConsumer">${searchWord.value}</div>
+    <div class="searchConsumer">${searchWord.value}</div>
+  </div>
+`;
 
-  const welcome = JSON.parse(useFetch("./markdown/_welcome.json").data);
-  
-  window.onpopstate = () => {
-    setCurrentBlog(new URLSearchParams(window.location.search).get("blog"));
-    document.title = welcome.name;
-  };
 
-  const { data, status } = useFetch("./markdown/_files_list.json");
-  if (status == "loading")
-    return (<div className="spinner"> <div></div> </div>);
-  if (status == "error" || !data)
-    return (<div className="error"> <div>&#x2716;</div> Oops! Something went wrong. </div>);
+document.querySelectorAll(".searchObserver").forEach(element => {
+  element.oninput = (event) => searchWord.update(event.target.value);
+});
 
-  const blogsList = JSON.parse(data);
-  const blogData = blogsList.find((blog) => blog.path == currentBlog);
+// document.querySelectorAll(".blogObserver").forEach((element) => {
+//   element.onclick = () => currentBlog.updater("");
+// });
 
-  let handleSearch = (query) => {
-    startTransition(() => setSearchWord(query.toLowerCase()));
-    setCurrentBlog(null);
-    window.scrollTo(0, 0);
-    history.pushState({}, "", window.location.pathname);
-    document.title = welcome.name;
-  };
+// function App() {
+//   const [currentBlog, setCurrentBlog] = useState(new URLSearchParams(window.location.search).get("blog"));
+//   const [searchWord, setSearchWord] = useState("");
 
-  let handleSelection = (path) => {
-    setCurrentBlog(path);
-    setSearchWord("");
-    window.scrollTo(0, 0);
-    history.pushState({ blog: path }, "", `?blog=${path}`);
-  };
+//   const welcome = JSON.parse(useFetch("./markdown/_welcome.json").data);
 
-  return (
-    <>
-      <Navigation {...{ searchWord, handleSearch }} />
+//   window.onpopstate = () => {
+//     setCurrentBlog(new URLSearchParams(window.location.search).get("blog"));
+//     document.title = welcome.name;
+//   };
 
-      { !currentBlog && !searchWord &&
-        <div className="content">
-          <h1> {welcome.heading} </h1>
-          <p> {welcome.line_1} </p>
-          <p> {welcome.line_2} </p>
-          <p> {welcome.line_3} </p>
-        </div> }
+//   const { data, status } = useFetch("./markdown/_files_list.json");
+//   if (status == "loading")
+//     return (<div className="spinner"> <div></div> </div>);
+//   if (status == "error" || !data)
+//     return (<div className="error"> <div>&#x2716;</div> Oops! Something went wrong. </div>);
 
-      { currentBlog && <Content {...{ blogData, handleSearch }} /> }
-      { !currentBlog && <Feed {...{ blogsList, handleSelection, searchWord }} /> }
-    </>
-  );
-}
+//   const blogsList = JSON.parse(data);
+//   const blogData = blogsList.find((blog) => blog.path == currentBlog);
 
-export default App;
-*/
+//   let handleSearch = (query) => {
+//     startTransition(() => setSearchWord(query.toLowerCase()));
+//     setCurrentBlog(null);
+//     window.scrollTo(0, 0);
+//     history.pushState({}, "", window.location.pathname);
+//     document.title = welcome.name;
+//   };
+
+//   let handleSelection = (path) => {
+//     setCurrentBlog(path);
+//     setSearchWord("");
+//     window.scrollTo(0, 0);
+//     history.pushState({ blog: path }, "", `?blog=${path}`);
+//   };
+
+//   return (
+//     <>
+//       <Navigation {...{ searchWord, handleSearch }} />
+
+//       { !currentBlog && !searchWord &&
+//         <div className="content">
+//           <h1> {welcome.heading} </h1>
+//           <p> {welcome.line_1} </p>
+//           <p> {welcome.line_2} </p>
+//           <p> {welcome.line_3} </p>
+//         </div> }
+
+//       { currentBlog && <Content {...{ blogData, handleSearch }} /> }
+//       { !currentBlog && <Feed {...{ blogsList, handleSelection, searchWord }} /> }
+//     </>
+//   );
+// }
+
+// export default App;
