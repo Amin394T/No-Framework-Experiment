1a69c4dde079faf468697447c664efb9bc56d2e8
Amin394 <amin394@outlook.com>
2024-09-16 10:33:06 +0100

    prevented static data from re-fetching when re-rendering

--- components/main.js
+++ components/main.js
@@ -3,13 +3,15 @@ import navigation from "./navigation.js";
 import content from "./content";
 import feed from "./feed";
 
+// ---------- STATE MANAGEMENT ---------- //
+
 let currentBlog = {
   value: new URLSearchParams(window.location.search).get("blog"),
   update: (blog) => {
+    history.pushState({ blog }, "", `?blog=${blog}`);
+
     searchQuery.value = "";
     currentBlog.value = blog;
-
-    history.pushState({ blog }, "", `?blog=${blog}`);
     render();
   },
 };
@@ -18,49 +20,50 @@ let searchQuery = {
   value: new URLSearchParams(window.location.search).get("search") ?? "",
   update: (query) => {
     query = query.toLowerCase();
-    searchQuery.value = query;
-    currentBlog.value = null;
-
     searchQuery.value && query
       ? history.replaceState({ query }, "", `?search=${query}`)
       : history.pushState({ query }, "", query ? `?search=${query}` : "/");
+
+    searchQuery.value = query;
+    currentBlog.value = null;
     render();
   },
 };
 
-// -------------------- //
+// ---------- STATIC FETCHING ---------- //
+
+let blogsList = [];
+let welcome;
+
+try {
+  document.querySelector("#root").innerHTML = `<div class="loading"> <div></div> </div>`;
+
+  let response = await fetch("./markdown/_welcome.json");
+  welcome = await response.json();
+
+  response = await fetch("./markdown/_files_list.json");
+  if (!response.ok)
+    throw new Error(`HTTP error! Status: ${response.status}`);
+  blogsList = await response.json();
+}
+catch (error) {
+  document.querySelector("#root").innerHTML =
+    `<div class="error"> <div>&#x2716;</div> Oops! Something went wrong. </div>`;
+  console.error(error);
+}
+
+let welcomeMessage = `
+  <div class="content">
+    <h1> ${welcome.heading} </h1>
+    <p> ${welcome.line_1} </p>
+    <p> ${welcome.line_2} </p>
+    <p> ${welcome.line_3} </p>
+  </div>`;
+
+// ---------- COMPONENTS RENDERING ---------- //
 
 const render = async () => {
-  let blogsList = [];
-  let welcome;
-
-  try {
-    document.querySelector("#root").innerHTML = `<div class="loading"> <div></div> </div>`;
-
-    let response = await fetch("./markdown/_welcome.json");
-    welcome = await response.json();
-
-    response = await fetch("./markdown/_files_list.json");
-    if (!response.ok)
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    blogsList = await response.json();
-  }
-  catch (error) {
-    document.querySelector("#root").innerHTML =
-      `<div class="error"> <div>&#x2716;</div> Oops! Something went wrong. </div>`;
-    console.error(error);
-  }
-
   let blogData = blogsList.find((blog) => blog.path == currentBlog.value);
-  let welcomeMessage = `
-    <div class="content">
-      <h1> ${welcome.heading} </h1>
-      <p> ${welcome.line_1} </p>
-      <p> ${welcome.line_2} </p>
-      <p> ${welcome.line_3} </p>
-    </div>`;
-
-  // -------------------- //
 
   if (!document.querySelector(".navigation"))
     document.querySelector("#root").insertAdjacentHTML("beforebegin", navigation());
@@ -77,7 +80,7 @@ const render = async () => {
 
   window.scrollTo(0, 0);
 
-  // -------------------- //
+  // ---------- LISTENERS ATTACHEMENT ---------- //
 
   setTimeout(() => {
     window.onpopstate = () => {
